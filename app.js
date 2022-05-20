1cb2efc3eb530f4a6e1b44a4d2046a1d8a493e10
Tomás Silva <al70680@alunos.utad.pt>
2022-05-20 11:11:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.static('/public'));
+
+app.get('/sounds/music.wav', function(req, res) {
+  res.sendFile(__dirname + '/sounds/music.wav');
+});
+
+app.get('/sounds/shoot.wav', function(req, res) {
+  res.sendFile(__dirname + '/sounds/shoot.wav');
+});
+
+app.get('/sounds/bounce.mp3', function(req, res) {
+  res.sendFile(__dirname + '/sounds/bounce.mp3');
+});
+
+app.get('/main.js', function(req, res) {
+  res.sendFile(__dirname + '/main.js');
+});
+
+app.get('/sound.js', function(req, res) {
+  res.sendFile(__dirname + '/sound.js');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html" );
+})
+
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port2 = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port2)
+})
